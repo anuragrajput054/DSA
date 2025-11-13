7a788afc5f4ab0505f408b7e375d1992c0209231
Anuragrajput054 <anuragsinghr054@gmail.com>
2025-11-14 00:02:12 +0530

    LC:198-house robber problem

--- DP/SubSuquenceProblem.java
+++ DP/SubSuquenceProblem.java
@@ -66,3 +66,47 @@ public class SubSuquenceProblem {
         raj(nums, ind + 1, k, s, list);
     }
 }
+
+// class Solution {
+// public int rob(int[] nums) {
+
+// Space optimization
+// if(nums == null || nums.length == 0) return 0;
+// if(nums.length == 1) return nums[0];
+// int pre1 = 0;
+// int pre2 = 0;
+// for(int i = 0; i < nums.length;i++){
+// int current = Math.max(nums[i] + pre2, pre1);
+// pre2 = pre1;
+// pre1 = current;
+// }
+// return pre1;
+
+// int ind = nums.length - 1;
+// int[] dp = new int[nums.length + 1];
+// Arrays.fill(dp,-1);
+// return helper(nums, ind, dp);
+
+// * tabulation
+// if(nums.length == 1) return nums[0];
+// int n = nums.length;
+// int[] dp = new int[n+1];
+// dp[0] = nums[0];
+// dp[1] = Math.max(nums[0], nums[1]);
+// for(int i = 2; i < n; i++){
+// dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+// }
+// return dp[n-1];
+// }
+
+// Memoization**
+// public static int helper(int[] nums , int ind, int[] dp){
+// if(ind == 0) return nums[0];
+// if(ind < 0) return 0;
+// if(dp[ind] != -1) return dp[ind];
+// int pick = helper(nums, ind - 2 , dp) + nums[ind];
+// int nonPick = helper(nums, ind -1, dp);
+// dp[ind] = Math.max(pick,nonPick);
+// return dp[ind];
+// }
+// }
