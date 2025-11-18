a79bfdba76f887f24d0ab79939e004b716aad278
Anuragrajput054 <anuragsinghr054@gmail.com>
2025-11-18 23:24:30 +0530

    Ninja traning problem

---
+++ DP/NinjaTraning.java
@@ -0,0 +1,29 @@
+public class NinjaTraning {
+    public static void main(String[] args) {
+        int[][] task = {
+                { 10, 40, 70 },
+                { 20, 50, 80 },
+                { 30, 60, 90 }
+        };
+        System.out.println(ninja(task.length - 1, 3, task));
+    }
+
+    public static int ninja(int day, int lastIndex, int[][] task) {
+        if (day == 0) {
+            int maxi = 0;
+            for (int i = 0; i < task[day].length; i++) {
+                if (i != lastIndex) {
+                    maxi = Math.max(maxi, task[0][i]);
+                }
+            }
+            return maxi;
+        }
+        int maxi = 0;
+        for (int i = 0; i < task[day].length; i++) {
+            if (i != lastIndex) {
+                maxi = Math.max(task[day][i] + ninja(day - 1, i, task), maxi);
+            }
+        }
+        return maxi;
+    }
+}
