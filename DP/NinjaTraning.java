f25eb7f2cd9e365438f1e427c8087adf03bc7b9e
Anuragrajput054 <anuragsinghr054@gmail.com>
2025-11-18 23:40:14 +0530

    Ninja memo

--- DP/NinjaTraning.java
+++ DP/NinjaTraning.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class NinjaTraning {
     public static void main(String[] args) {
         int[][] task = {
@@ -6,6 +8,9 @@ public class NinjaTraning {
                 { 30, 60, 90 }
         };
         System.out.println(ninja(task.length - 1, 3, task));
+        int[][] dp = new int[task.length][4];
+        Arrays.fill(dp[0], -1);
+        System.out.println(ninjaMemo(task.length - 1, 3, task, dp));
     }
 
     public static int ninja(int day, int lastIndex, int[][] task) {
@@ -25,5 +30,31 @@ public class NinjaTraning {
             }
         }
         return maxi;
+    }
+
+    public static int ninjaMemo(int day, int lastIndex, int[][] task, int[][] dp) {
+        if (day == 0) {
+            if (dp[day][lastIndex] != -1) {
+                return dp[day][lastIndex];
+            }
+            int maxi = 0;
+            for (int i = 0; i < task[day].length; i++) {
+                if (i != lastIndex) {
+                    maxi = Math.max(maxi, task[0][i]);
+                }
+            }
+            return dp[day][lastIndex] = maxi;
+        }
+
+        if (dp[day][lastIndex] != -1) {
+            return dp[day][lastIndex];
+        }
+        int maxi = 0;
+        for (int i = 0; i < task[day].length; i++) {
+            if (i != lastIndex) {
+                maxi = Math.max(task[day][i] + ninjaMemo(day - 1, i, task, dp), maxi);
+            }
+        }
+        return dp[day][lastIndex] = maxi;
     }
 }
